8480b5971d28cb4d30c01d7b5816f0d2e5c5c89f
congle-engineer <congle-engineer@gmail.com>
2024-07-09 14:24:57 +0700

    Add existing project files to Git

---
+++ mapping/transfer.ts
@@ -0,0 +1,17 @@
+import { Exchange } from '../generated/schema';
+import { Transfer } from '../generated/transfer/USDT';
+
+export function handleTransfer(event: Transfer) : void {
+  let transfer = Exchange.load(event.transaction.hash.toHex());
+
+  if (transfer === null) {
+    transfer = new Exchange(event.transaction.hash.toHex());
+  }
+
+  transfer.block = event.block.number;
+  transfer.from = event.params.from.toHex();
+  transfer.to = event.params.to.toHex();
+  transfer.amount = event.params.value;
+
+  transfer.save();
+}
